6ff6b035834b5e080fc504fef3a34591bf670771
Ryan Kooy <rwkooy@gmail.com>
2019-09-02 12:39:18 -0400

    concert-this functional

--- liri.js
+++ liri.js
@@ -4,29 +4,69 @@ var moment = require('moment');
 require('dotenv').config();
 var keys = require('./keys.js');
 // var db = require('db');
-
 var spotify = new Spotify(keys.spotify);
 
-spotify.search({
-    type: ,
-    query:
-}, function(err, data) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log(data);
-});
+var command = process.argv[2];
 
-axios.get().then(function(resp) {
-    console.log(resp);
-}).catch(function(err) {
-    return console.log(err);
-}).finally(function() {
+switch(command) {
+    case 'concert-this':
+        concertThis();
+        break;
+    case 'spotify-this-song':
+        spotifyThis();
+        break;
+    case 'movie-this':
+        movieThis();
+        break;
+    case 'do-what-it-says':
+        doThis();
+        break;
+}
 
-});
+// function spotifyThis() {
+//     spotify.search({
+//         type: ,
+//         query:
+//     }, function(err, data) {
+//         if(err) {
+//             return console.log(err);
+//         }
+//         console.log(data);
+//     });
+// }
 
-// db.connect({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS
-// });
+function concertThis() {
+    var artist = process.argv[3];
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function(resp) {
+            for (var i = 0; i < 20; i++) {
+                console.log('Venue: ' + resp.data[i].venue.name);
+                console.log('Location: ' + resp.data[i].venue.city + ', ' + resp.data[i].venue.country);
+                console.log('Date: ' + resp.data[i].datetime);
+                console.log('--------------------------------------------');
+            }
+        })
+        .catch(function(err) {
+            if(err.resp) {
+                console.log("---------------Data---------------");
+                console.log(err.resp.data);
+                console.log("---------------Status---------------");
+                console.log(err.resp.status);
+                console.log("---------------Status---------------");
+                console.log(err.resp.headers);
+            } else if(err.request) {
+                console.log(err.request);
+            } else {
+                console.log('Error', err.message);
+            }
+            console.log(err.config);
+        });
+}
+
+function movieThis() {
+
+}
+
+function doThis() {
+
+}
